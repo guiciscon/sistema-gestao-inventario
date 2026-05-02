9dc5b7ff854f34f3fcba7a449f5d1f41d15d7da6
Luis Guilherme Moreira Ciscon <ciscon.moreira@gmail.com>
2026-05-02 10:02:48 -0300

    feat - finalização CRUD + READ.ME
    Metodos PUT e DELETE finalizados e testados via terminal
    READ.ME com comandos finalizados para testar a aplicação totalmente via terminal  - a ser atualizado futuramente

--- README.md
+++ README.md
@@ -1,2 +1,40 @@
 # sistema-gestao-inventario
 Sistema de gestão de inventário fullstack.
+
+### COMANDOS ###
+**ABRIR TABELA NO NAVEGADOR (ENQUANTO NAO EXISTE FRONTEND É BOM)**
+npx prisma studio 
+
+**RODA O SISTEMA E ATUALIZA AUTOMATICAMENTE**
+npm run dev 
+
+**SCRIPT PARA ADICIONAR PRODUTAS NA LISTA (VIA TERMINAL)**
+Invoke-RestMethod -Uri http://localhost:3001/products/insertproducts `
+-Method POST `
+-Headers @{ "Content-Type" = "application/json" } `
+-Body '{
+  "name": "Teste",
+  "description": "",
+  "price": 0.00,
+  "stock_quantity": -2
+}'
+
+**COMANDO PARA RETORNAR TABELA VIA TERMINAL**
+Invoke-RestMethod -Uri http://localhost:3001/products -Method GET
+
+**COMANDO PARA RETORNAR ITEM VIA TERMINAL (SUBSTITUIR ASTERISTICOS PELO ID DO ITEM)**
+Invoke-RestMethod -Uri http://localhost:3001/products/** -Method GET
+
+**COMANDO PARA ATUALIZAR ITEM (SUBSTITUIR ASTERISTICO PELO ID DO ITEM)**
+Invoke-RestMethod -Uri http://localhost:3001/products/edit/** `
+-Method PUT `
+-Headers @{ "Content-Type" = "application/json" } `
+-Body '{
+  "name": "Mouse atualizado",
+  "description": "Nova descrição",
+  "price": 200,
+  "stock_quantity": 20
+}'
+
+c**COMANDO PARA DELETAR ITEM (SUBSTITUIR ASTERISTICO PELO ID DO ITEM)**
+Invoke-RestMethod -Uri http://localhost:3001/products/delete/** -Method DELETE
--- backend/src/routes/methods.ts
+++ backend/src/routes/methods.ts
@@ -9,12 +9,7 @@ router.post('/insertproducts', async (req, res) => {
 
     try {
         const Product = await prisma.products.create({
-            data:{
-                name,
-                description,
-                price,
-                stock_quantity
-            }
+            data:{name, description, price, stock_quantity}
         });
     res.json(Product);
 } catch (error) {
@@ -33,5 +28,49 @@ router.get('/', async (req, res) => {
         res.status(500).json({ error: 'Erro ao listar produtos' });
     }
 });
+
+//metodo para retornar um produto
+router.get('/:id', async (req, res) =>{
+    const {id} = req.params;
+    try {
+        const product = await prisma.products.findUnique({
+            where: {id: id}
+        });
+        res.json(product);
+    } catch (error) {
+        console.error(error);
+        res.status(500).json({ error: 'Erro ao buscar produto' });
+    }
+})
+
+//editando conteudo do produto
+router.put('/edit/:id', async (req, res) => {
+    const {id} = req.params;
+    const {name, description, price, stock_quantity} = req.body;
+    try {
+        const Product = await prisma.products.update({
+            where: {id:id},
+            data: req.body
+        });
+    res.json(Product);
+} catch (error) {
+    console.error(error);
+    res.status(500).json({ error: 'Erro ao atualizar produto' });
+}
+})
+ 
+//metodo para deletar
+router.delete('/delete/:id', async (req, res) => {
+    const {id} = req.params;
+    try {
+        const Product = await prisma.products.delete({
+            where: {id:id}
+        });
+        res.json(Product);
+    } catch (error) {
+        console.error(error);
+        res.status(500).json({ error: 'Erro ao deletar produto' });
+    }
+})
 
 export default router;
